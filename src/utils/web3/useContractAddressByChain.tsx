de5e805d56fa00e6c4e67530c29979909106099d
coffee419 <benjaminmoritz8888@gmail.com>
2024-03-07 01:31:45 +0530

    conflict resolved

---
+++ src/utils/web3/useContractAddressByChain.tsx
@@ -0,0 +1,24 @@
+import { useAccount } from "wagmi";
+export const useContractAddressByChain = () => {
+  const { chain } = useAccount();
+  const chainID = chain?.id;
+  let registryAddress: string | undefined;
+  switch (chainID) {
+    case 80001:
+      registryAddress = process.env.NEXT_PUBLIC_REGISTRY_ADDRESS_POLYGON_MUMBAI;
+      break;
+    case 204:
+      registryAddress = process.env.NEXT_PUBLIC_REGISTRY_ADDRESS_OPBNB_TESTNET;
+      break;
+    case 195:
+      registryAddress = process.env.NEXT_PUBLIC_REGISTRY_ADDRESS_X1_TESTNET;
+      break;
+    case 80085:
+      registryAddress = process.env.NEXT_PUBLIC_REGISTRY_ADDRESS_BERACHAIN_ARTIO;
+      break;
+    default:
+      console.error("Chain not supported");
+      break;
+  }
+  return registryAddress;
+};
