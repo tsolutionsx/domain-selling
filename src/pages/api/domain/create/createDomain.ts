cc56519d0f8b0ffa605204f00a9cb13b17d15665
coffee419 <benjaminmoritz8888@gmail.com>
2024-03-08 19:28:25 +0530

    Add createDomain API endpoint

---
+++ src/pages/api/domain/create/createDomain.ts
@@ -0,0 +1,38 @@
+import { PrismaClient, Chain } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+// pages/api/createEntry.js
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    try {
+      // Parse incoming data from request body
+      const { domainName, userID } = req.body;
+
+      //   Create domain entry
+      const domain = await prisma.domain.create({
+        data: {
+          domainName: domainName,
+          User: { connect: { id: userID } }
+        }
+      });
+
+      //   //   Create domain entry
+      //   const domain = await prisma.domain.create({
+      //     data: {
+      //       domainName: "devtest123qwerty1231",
+      //       User: { connect: { id: 9 } }
+      //     }
+      //   });
+
+      res.status(201).json({ message: "Entry created successfully", data: { domain } });
+    } catch (error) {
+      console.error("Error creating entry:", error);
+      res.status(500).json({ error: "Could not create entry" });
+    }
+  } else {
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
+}
