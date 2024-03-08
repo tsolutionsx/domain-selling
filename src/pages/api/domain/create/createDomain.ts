ee8a5b1c456ae550b2840fa1f81d42cde7260d0b
coffee419 <benjaminmoritz8888@gmail.com>
2024-03-08 19:28:46 +0530

    Remove unused import statement

--- src/pages/api/domain/create/createDomain.ts
+++ src/pages/api/domain/create/createDomain.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Chain } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 // pages/api/createEntry.js
